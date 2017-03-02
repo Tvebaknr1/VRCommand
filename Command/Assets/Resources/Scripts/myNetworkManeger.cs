87868373715fa9a8081d6ca60462b207312ddfb0
DESKTOP-L3B06SV\Admin <emil@indeni.dk>
2017-03-02 13:44:11 +0100

    no message

--- Command/Assets/Resources/Scripts/myNetworkManeger.cs
+++ Command/Assets/Resources/Scripts/myNetworkManeger.cs
@@ -12,12 +12,12 @@ public class myNetworkManeger : NetworkManager
         PlayerOptions Options = gameObject.GetComponent<PlayerOptions>();
         if (i == 0)
         {
-            player = Instantiate(Resources.Load("Players/testPlayer"), transform.position, Quaternion.identity) as GameObject;
+            player = Instantiate(Resources.Load("Players/VRPlayer"), transform.position, Quaternion.identity) as GameObject;
             i++;
         }
         else 
         {
-            player = Instantiate(Resources.Load("Players/VRPlayer"), transform.position, Quaternion.identity) as GameObject;
+            player = Instantiate(Resources.Load("Players/testPlayer"), transform.position, Quaternion.identity) as GameObject;
         }
             
 
