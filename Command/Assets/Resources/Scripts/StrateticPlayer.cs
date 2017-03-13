6063f7d7f92abc0d06c226a28a466bcfbca3e771
Lange-Laptop-Pc\Lange-Laptop <peterleolange@yahoo.dk>
2017-03-13 12:17:30 +0100

    temp fix to stratetic player

--- Command/Assets/Resources/Scripts/StrateticPlayer.cs
+++ Command/Assets/Resources/Scripts/StrateticPlayer.cs
@@ -13,13 +13,17 @@ public class StrateticPlayer : MonoBehaviour
         temp.name = "Fuck unity and camaras";
         temp.AddComponent<Camera>();
         temp.transform.SetParent(this.transform);
-        temp.transform.position = new Vector3(0, 0, 0);
-        temp.transform.rotation = new Quaternion(0, 0, 90, 0);
+        temp.transform.position = new Vector3(0, 10, 0);
+        temp.transform.rotation = Quaternion.Euler(90,0,0);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(movement == null)
+        {
+            movement = GameObject.FindGameObjectWithTag("VRPlayer").GetComponent<playerMovement>();
+        }
         if (Input.GetMouseButtonDown(0))
         {
             RaycastHit hit;
