51d7fd800935ccb49c292d3df977fac07fd77e8d
DESKTOP-L3B06SV\Admin <emil@indeni.dk>
2017-03-30 09:35:45 +0200

    more fix

--- Command/Assets/fixmultiplayer.cs
+++ Command/Assets/fixmultiplayer.cs
@@ -6,17 +6,16 @@ public class fixmultiplayer : MonoBehaviour {
 
 	// Use this for initialization
 	void Awake () {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
         Transform[] temp = this.transform.GetComponentInChildren<MeshRenderer>(true).GetComponent<Transform>().GetComponentsInChildren<Transform>(true);
         foreach (Transform tra in temp)
         {
             tra.gameObject.SetActive(true);
         }
-        Debug.Log(temp);
-    }
-	
-	// Update is called once per frame
-	void Update () {
-        
 
     }
 }
