7271022a8dc4273a10db7b3076fa993398a29d8b
DESKTOP-L3B06SV\Admin <emil@indeni.dk>
2017-04-27 13:51:04 +0200

    spawning object on gmaeobejct

--- Command/Assets/shot.cs
+++ Command/Assets/shot.cs
@@ -5,13 +5,12 @@ using UnityEngine.Networking;
 
 public class shot : NetworkBehaviour {
     private LayerMask shootMask;
-    private GameObject bullet;
+    public GameObject bullet;
     [Command]
     public void CmdShoot(Vector3 startingPoint, Quaternion rotation, Vector3 direction, int bulletSpeed)
     {
         Debug.Log("fuck dis");
-        gun gun = this.transform.GetComponentInChildren<MeshRenderer>(true).GetComponent<Transform>().GetComponentInChildren<gun>();
-        this.bullet = gun.bullet;
+        
         //GameObject temp = (GameObject)Network.Instantiate(this.bullet, startingPoint, rotation, 0);
         GameObject temp = Instantiate(this.bullet, startingPoint, rotation);
         temp.GetComponent<Bullet>().setTarget(direction, bulletSpeed, shootMask);
