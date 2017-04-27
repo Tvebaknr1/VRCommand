b1b0a7df683d44961ac802d7b6eb9e8e40a2adf5
DESKTOP-L3B06SV\Admin <emil@indeni.dk>
2017-04-27 13:40:26 +0200

    removed gameobject in peramenter in shot

--- Command/Assets/shot.cs
+++ Command/Assets/shot.cs
@@ -7,15 +7,15 @@ public class shot : NetworkBehaviour {
     private LayerMask shootMask;
     private GameObject bullet;
     [Command]
-    public void CmdShoot(Vector3 startingPoint, Quaternion rotation, Vector3 direction, GameObject bullet, int bulletSpeed)
+    public void CmdShoot(Vector3 startingPoint, Quaternion rotation, Vector3 direction, int bulletSpeed)
     {
         Debug.Log("fuck dis");
         gun gun = this.transform.GetComponentInChildren<MeshRenderer>(true).GetComponent<Transform>().GetComponentInChildren<gun>();
         this.bullet = gun.bullet;
-        GameObject temp = (GameObject)Network.Instantiate(this.bullet, startingPoint, rotation, 0);
-        //GameObject temp = GameObject.Instantiate(this.bullet, startingPoint, rotation);
+        //GameObject temp = (GameObject)Network.Instantiate(this.bullet, startingPoint, rotation, 0);
+        GameObject temp = GameObject.Instantiate(this.bullet, startingPoint, rotation);
         temp.GetComponent<Bullet>().setTarget(direction, bulletSpeed, shootMask);
         
-        //NetworkServer.SpawnWithClientAuthority(temp,netID.connectionToClient);
+        NetworkServer.Spawn(temp);
     }
 }
