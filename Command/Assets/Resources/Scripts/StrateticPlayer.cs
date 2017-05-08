701d40480f03ba88392fd0e649bb7e555c03278e
Lange-Laptop-Pc\Lange-Laptop <peterleolange@yahoo.dk>
2017-05-08 14:14:33 +0200

    added networkbehavor

--- Command/Assets/Resources/Scripts/StrateticPlayer.cs
+++ Command/Assets/Resources/Scripts/StrateticPlayer.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 
-public class StrateticPlayer : MonoBehaviour
+public class StrateticPlayer : NetworkBehaviour
 {
     public playerMovement movement;
     // Use this for initialization
